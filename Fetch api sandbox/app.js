1c6d4458fc86b92aabeb4d0c980e656c4ded1626
kihara Nelson <rickymburu38@gmail.com>
2022-09-16 15:10:06 +0300

    fetch api

---
+++ Fetch api sandbox/app.js
@@ -0,0 +1,58 @@
+document.getElementById('button1').addEventListener('click', getText);
+
+document.getElementById('button2').addEventListener('click', getJson);
+
+document.getElementById('button3').addEventListener('click', getExternal);
+
+//get local text data
+function getText(){
+  fetch('text.txt')
+  .then(function(res){
+    return res.text(); 
+  })
+  .then(function(data){
+    console.log(data)
+    document.getElementById('output').innerHTML=data;
+  })
+  .catch(function(err){
+    console.log(err)
+  })
+}
+
+//get local Json data
+function getJson(){
+  fetch('posts.json')
+  .then(function(res){
+    return res.json(); 
+  })
+  .then(function(data){
+    console.log(data)
+    let output='';
+    data.forEach(function(post) {
+      output+=`<li>${post.title}</li>`;
+    });
+    document.getElementById('output').innerHTML=output;
+  })
+  .catch(function(err){
+    console.log(err)
+  })
+}
+
+//get from External Api
+function getExternal(){
+  fetch('https://api.github.com/users')
+  .then(function(res){
+    return res.json(); 
+  })
+  .then(function(data){
+    console.log(data)
+    let output='';
+    data.forEach(function(user) {
+      output+=`<li>${user.login}</li>`;
+    });
+    document.getElementById('output').innerHTML=output;
+  })
+  .catch(function(err){
+    console.log(err)
+  })
+}
