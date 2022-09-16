6704c89a552e42c87743b76c81786b1c64ba6cb7
kihara Nelson <rickymburu38@gmail.com>
2022-09-16 15:36:22 +0300

    arrow functions

---
+++ Arrow Functions/app.js
@@ -0,0 +1,16 @@
+//  const sayHello=function(){
+//   console.log('upload nelki')
+//  }
+//  const sayHello=()=>{
+//   console.log('upload nelki')
+//  }
+
+// //one line function does not need braces
+//  const sayHello=()=>console.log('upload nelki');
+// //one line return
+// const sayHello=()=>'hello';
+// //return Object
+// const sayHello=()=>({msg:'nelki'})
+//single param does not need parenthesis
+const sayHello=name=>console.log(`hello ${name}`);
+sayHello('nelki')
